1e4a05bc3379812e6be8967e362c9f04e98f929e
easylearningbd <elebdsoft@gmail.com>
2025-12-13 04:18:04 +0600

    182 Setup User & Coaches Chat System Part 2

--- app/Http/Controllers/User/CoachSessionController.php
+++ app/Http/Controllers/User/CoachSessionController.php
@@ -11,6 +11,7 @@ use App\Models\UserCoachProfile;
 use App\Services\CoachService;
 use Illuminate\Support\Facades\Auth;
 use App\Models\UserGoal;
+use Illuminate\Support\Facades\DB;
 
 class CoachSessionController extends Controller
 {
@@ -65,6 +66,53 @@ class CoachSessionController extends Controller
                 'error' => $e->getMessage()
             ]);
         }
+    }
+    //End Method 
+
+
+    public function CoachesSendMessage(Request $request,Coach $coach){
+
+        $request->validate([
+            'message' => 'required|string',
+            'session_id' => 'required'
+        ]);
+
+        $user = Auth::user();
+
+        try {
+
+            DB::beginTransaction();
+
+            // Get session
+            $session = CoachSession::where('session_id',$request->session_id)
+                        ->where('user_id',$user->id)
+                        ->where('coach_id',$coach->id)
+                        ->where('is_active', true)
+                        ->firstOrFail();
+    // save user message
+    $userMessage = $this->saveMessage($session, 'user',$request->message);
+
+    // Get Ai Response 
+    $aiResponse = $this->coachService->sendMessage($session,$request->message);
+
+    // Save Ai Message
+    $aiMessage = $this->saveMessage($session, 'assistant',$aiResponse['message']);
+
+    DB::commit();
+
+    return response()->json([
+        'success' => true,
+        'user_message' => $userMessage,
+        'ai_message' => $aiResponse['tokens_used'],
+        'session' => $session->fresh()
+    ]);         
+
+        } catch (\Exception $e) {
+           return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+
     }
     //End Method 
 
